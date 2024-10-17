232b0a0f692ef28c6c2f202a1b6aa4fa96ef103f
dokee <dokee.39@gmail.com>
2024-10-18 06:49:14 +0800

    update socket

--- src/util.cc
+++ src/util.cc
@@ -1,5 +1,7 @@
 #include <chrono>
 #include <unistd.h>
+#include <iostream>
+#include <cstring>
 #include "util.hpp"
 
 namespace util {
@@ -10,5 +12,31 @@ double getTime() {
     double t = duration.count() / 1000000.0f;
 
     return t;
+}
+
+in_addr_t to_in_addr(const std::string &host) {
+    in_addr_t ip;
+    addrinfo hints, *res;
+    int status;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
+
+    if ((status = getaddrinfo(host.c_str(), nullptr, &hints, &res)) != 0) {
+        std::cerr << "getaddrinfo error: " << gai_strerror(status) << std::endl;
+        return INADDR_NONE;
+    }
+
+    sockaddr_in* ipv4 = reinterpret_cast<sockaddr_in *>(res->ai_addr);
+    ip = ipv4->sin_addr.s_addr;
+
+    if (res->ai_next != nullptr) {
+        std::clog << R"(The host ")" + host + R"(" corresponds to multiple IP addresses)";
+    }
+
+    freeaddrinfo(res);
+
+    return ip;
 }
 }
--- src/util.hpp
+++ src/util.hpp
@@ -1,10 +1,14 @@
 #pragma once
 
-#include <iostream>
+#include <string>
 #include <type_traits>
+#include <netdb.h>
 
 namespace util {
 double getTime();
+
+/**socket**/
+in_addr_t to_in_addr(const std::string &host);
 
 /**iostream**/
 template<typename T, typename = void>
